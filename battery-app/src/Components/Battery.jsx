a662bc250e3013bc827c1290088c30d4a9721d61
Poojitha <poojithaglok@gmail.com>
2025-04-05 17:29:46 +0530

    Add BatteryStatus-Api

---
+++ battery-app/src/Components/Battery.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import "./Battery.css"
+import { BatteryContext } from './BatteryContext';
+const Battery = () => {
+    const{battery,charging,onFetch} = useContext(BatteryContext);
+     return (
+    <div> 
+       <h3>BATTERY LEVEL</h3>
+      <h3>{battery * 100  }%</h3>
+      <h3>{charging ? "Device is Charging" : "Device is not Charging"}</h3>
+       
+    <div className='BatteryContainer'>
+      <button className='battery' onClick={onFetch}> Battery %</button>
+      
+        </div>
+    </div>
+  )
+}
+
+export default Battery
